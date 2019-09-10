2979803c0be4250f08a66496ed932adf90261a40
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2019-09-10 15:43:14 +0200

    Lade till frågan om ålder samt justerade lite i koden.

--- Hemläxor/v37/basics.java
+++ Hemläxor/v37/basics.java
@@ -4,5 +4,15 @@ public class basics {
 			public static void main(String[] args) {
 				
 				Scanner input = new Scanner(System.in);
+				
+				System.out.println("Vad heter du?");
+				
+				String namn = input.nextLine();
+				
+				System.out.println("Hur gammal är du?");
+				
+				String alder = input.nextLine();
+				
+				
 			}
 }
