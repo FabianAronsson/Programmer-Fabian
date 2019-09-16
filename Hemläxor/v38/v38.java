38a68b7aa6c777e6918da7c384dc728c01cb3b30
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2019-09-16 15:40:33 +0200

    Skapade en array samt variabler.
    Tror inte detta är korrekt sätt att göra det på, kommer antagligen ändra en del.

---
+++ Hemläxor/v38/v38.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class v38 {
+		 public static void main(String[] args) {
+			 		System.out.println("Skriv in 10 hela tal");
+			 		Scanner input = new Scanner(System.in);
+			 		int s1 = input.nextInt();
+			 		int s2 = input.nextInt();
+			 		int s3 = input.nextInt();
+			 		int s4 = input.nextInt();
+			 		int s5 = input.nextInt();
+			 		int s6 = input.nextInt();
+			 		int s7 = input.nextInt();
+			 		int s8 = input.nextInt();
+			 		int s9 = input.nextInt();
+			 		int s10 = input.nextInt();
+			 		
+			 		
+			 		
+			 		int[] siffror = {s1, s2, s3, s4, s5, s6, s7, s8, s9, s10};
+			 		Arrays.sort(siffror);
+			 		
+			 		/*System.out.println(siffror[0]);
+			 		System.out.println(siffror[siffror.length-1]); Fattar inte detta, vad betyder 0 i detta sammanhanget?
+
+			 		/*System.out.println(Arrays.toString(siffror));*/
+			 		
+			 		
+			 		
+		}
+}
