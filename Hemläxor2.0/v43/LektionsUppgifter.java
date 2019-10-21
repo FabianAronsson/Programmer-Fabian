4f6c8548c0e4f56b83452d833a5163c9cd787101
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2019-10-21 11:30:00 +0200

    Skapade en konvertering av kelvin.

--- Hemläxor2.0/v43/LektionsUppgifter.java
+++ Hemläxor2.0/v43/LektionsUppgifter.java
@@ -3,32 +3,45 @@ import java.util.Scanner;
 public class LektionsUppgifter {
 
 	public static void main(String[] args) {
-		lol();
-
-		Scanner in = new Scanner(System.in);
-		min(in.nextInt(), in.nextInt());
-
+		
+		
+		kelvin(32);
 	}
 
 	/**
 	 * Skriver ut texten HAHAHAHAHAHAHA.
 	 */
 	public static void lol() {
-		System.out.println("HAHAHAHAHAHAHA");
+		System.out.println("HAHAHAHAHAHAHA.");
+	}
+
+	/**
+	 * Denna metod skriver ut det minsta värdet.
+	 * 
+	 * @param a = a är indata för en int a
+	 * @param b = b är indata för en int b
+	 */
+	public static void min(int a, int b) {
+		if (a < b) {
+			System.out.println(a);
+		} else {
+			System.out.println(b);
+		}
 	}
 	
 	/**
-	 * Denna metod skriver ut 
+	 * Denna metod skriver ut talen från 1 till n. 
 	 * 
-	 * @param a
-	 * @param b
+	 * @param n = där n är en int som matas in av användaren.
 	 */
-	public static void min(int a, int b) {
-			if(a < b) {
-				System.out.println(a);
-			}
-			else {
-				System.out.println(b);
-			}
+	public static void count(int n) {
+		for (int i = 1; i <= n; i++) {
+			System.out.println(i);
 		}
+	}
+	
+	public static double kelvin(double celsius) {
+		double kelvin = celsius + 273.15;
+		return kelvin;
+	}
 }
