8fba5ae7429bd16533347518235a7ad249cc877c
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2019-10-21 11:05:25 +0200

    Skapade 2 metoder

--- Hemläxor2.0/v43/LektionsUppgifter.java
+++ Hemläxor2.0/v43/LektionsUppgifter.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
 
 public class LektionsUppgifter {
 
 	public static void main(String[] args) {
 		lol();
 
+		Scanner in = new Scanner(System.in);
+		min(in.nextInt(), in.nextInt());
+
+	}
+
+	/**
+	 * Skriver ut texten HAHAHAHAHAHAHA.
+	 */
+	public static void lol() {
+		System.out.println("HAHAHAHAHAHAHA");
 	}
 	
-		public static void lol() {
-			System.out.println("HAHAHAHAHAHAHA");
+	/**
+	 * Denna metod skriver ut 
+	 * 
+	 * @param a
+	 * @param b
+	 */
+	public static void min(int a, int b) {
+			if(a < b) {
+				System.out.println(a);
+			}
+			else {
+				System.out.println(b);
+			}
 		}
 }
